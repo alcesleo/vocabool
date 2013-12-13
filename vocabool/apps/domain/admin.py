7b29fa51a5706d895bb6a492abbf35ea0fcd98a8
alcesleo <lagginglion@gmail.com>
2013-12-13 21:26:26 +0100

    Some admin display options

--- vocabool/apps/domain/admin.py
+++ vocabool/apps/domain/admin.py
@@ -1,7 +1,21 @@
 from django.contrib import admin
 from vocabool.apps.domain.models import Vocabulary, Term, Clarification, Userterm
 
+class TermAdmin(admin.ModelAdmin):
+    list_display = ('text', 'language')
+    search_fields = ('text',)
+
+admin.site.register(Term, TermAdmin)
+
+
+class UsertermAdmin(admin.ModelAdmin):
+    list_display = ('term', 'vocabulary', 'created')
+    list_filter = ('created',)
+    search_fields = ('text',)
+    ordering = ('-created',)
+
+admin.site.register(Userterm, UsertermAdmin)
+
+
 admin.site.register(Vocabulary)
-admin.site.register(Term)
 admin.site.register(Clarification)
-admin.site.register(Userterm)
