9b3a52bd3a6e6e57ff6a93761066afd51e5091d7
alcesleo <lagginglion@gmail.com>
2013-12-13 20:00:12 +0100

    Unique constraint and default english

--- vocabool/apps/domain/models.py
+++ vocabool/apps/domain/models.py
@@ -18,7 +18,10 @@ class Vocabulary(models.Model):
 class Term(models.Model):
     text = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
-    language = models.CharField(max_length=2) # ISO 639-1 language code
+    language = models.CharField(max_length=2, default='en') # ISO 639-1 language code
+
+    class Meta:
+        unique_together = ('text', 'language') # TODO: case insensitive
 
     def __str__(self):
         return ellipsify(self.text)
