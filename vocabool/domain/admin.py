027e26f7653b3d98e6c396146e827bfa85e1fe96
alcesleo <lagginglion@gmail.com>
2013-12-14 19:26:04 +0100

    Reorder

--- vocabool/domain/admin.py
+++ vocabool/domain/admin.py
@@ -5,8 +5,6 @@ class TermAdmin(admin.ModelAdmin):
     list_display = ('text', 'language')
     search_fields = ('text',)
 
-admin.site.register(Term, TermAdmin)
-
 class ListemeAdmin(admin.ModelAdmin):
     list_display = ('term', 'created', 'owner')
     list_filter = ('created',)
@@ -14,22 +12,23 @@ class ListemeAdmin(admin.ModelAdmin):
     ordering = ('-created',)
     filter_horizontal = ('clarifications',)
 
-admin.site.register(Listeme, ListemeAdmin)
-
 class ClarificationAdmin(admin.ModelAdmin):
     list_display = ('text', 'term', 'language', 'category', 'created')
     ordering = ('term',)
-
-admin.site.register(Clarification, ClarificationAdmin)
 
 class VocabularyAdmin(admin.ModelAdmin):
     list_display = ('name', 'count', 'owner', 'created')
     filter_horizontal = ('listemes',)
 
     # show only this users listemes
+    # >1.6.1 https://code.djangoproject.com/ticket/21593
     def formfield_for_manytomany(self, db_field, request, **kwargs):
         if db_field.name == 'listemes':
             kwargs['queryset'] = Listeme.objects.filter(owner=1)
         return super(VocabularyAdmin, self).formfield_for_manytomany(db_field, request, **kwargs)
 
+
+admin.site.register(Term, TermAdmin)
+admin.site.register(Listeme, ListemeAdmin)
+admin.site.register(Clarification, ClarificationAdmin)
 admin.site.register(Vocabulary, VocabularyAdmin)
