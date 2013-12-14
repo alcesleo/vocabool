6453ef2d98e48c45fc9fcc61305299338397c35c
alcesleo <lagginglion@gmail.com>
2013-12-14 19:17:08 +0100

    Somewhat functioning api

--- vocabool/service/resources.py
+++ vocabool/service/resources.py
@@ -1,14 +1,23 @@
 from tastypie import fields
 from tastypie.resources import ModelResource
-from vocabool.domain.models import Vocabulary, Listeme
+from vocabool.domain.models import Vocabulary, Listeme, Clarification
+
+
+
+class ClarificationResource(ModelResource):
+    class Meta:
+        queryset = Clarification.objects.all()
+        resource_name = 'clarification'
 
 class ListemeResource(ModelResource):
+    clarifications = fields.ManyToManyField(ClarificationResource, 'clarifications')
     class Meta:
         queryset = Listeme.objects.all()
         resource_name = 'listeme'
 
 class VocabularyResource(ModelResource):
-    listeme = fields.ToManyField(ListemeResource, 'listemes', full_list=False, full_detail=True)
+    listeme = fields.ManyToManyField(ListemeResource, 'listemes', full=True)
     class Meta:
         queryset = Vocabulary.objects.all()
         resource_name = 'vocabulary'
+        # filtering = { 'title': tastypie.constants.ALL }
