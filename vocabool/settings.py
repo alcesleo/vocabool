4d27297cd34ff86430350bea78b18c5966cdaea1
alcesleo <lagginglion@gmail.com>
2013-12-20 11:16:10 +0100

    django-bower functioning

--- vocabool/settings.py
+++ vocabool/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
 
     'south',
+    'djangobower',
 
     'vocabool.domain',
 )
@@ -82,6 +83,23 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), ".."),
+)
 
 STATIC_URL = '/static/'
+
+BOWER_COMPONENTS_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'djangobower.finders.BowerFinder',
+)
+
+BOWER_INSTALLED_APPS = (
+    'underscore',
+    'jquery',
+    'backbone',
+)
