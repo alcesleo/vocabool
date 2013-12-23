f3c23aef756ad2619bebb37e602865e7fc03f638
alcesleo <lagginglion@gmail.com>
2013-12-23 15:13:55 +0100

    Much cleaner translation service

--- vocabool/api/service.py
+++ vocabool/api/service.py
@@ -11,29 +11,46 @@ class Service():
         # kwargs['definitions'].setdefault(WikipediaAPI)
         # kwargs['translations'].setdefault(YandexTranslateAPI)
 
-        self.definitions = GoogleDictionaryAPI()
-        self.translations = YandexTranslateAPI()
+        self.definitions_api = GoogleDictionaryAPI()
+        self.translation_api = YandexTranslateAPI()
 
 
-    def get_translation(self, text, from_language, to_language):
+    def _database_translate(self, text, from_language, to_language):
+        """Get translation from DB, returns None if none exist."""
         # get the first object matching, will not throw
         translations = Translation.objects.filter(text=text,
-                                              from_language=from_language,
-                                              to_language=to_language)
+                                                  from_language=from_language,
+                                                  to_language=to_language)
         if translations:
-            translation_obj = translation[0]
-        else:
-            translations = self.translations.translate(text, from_language, to_language)
+            return translations[0]
+        return None
 
-            # TODO: factory function
-            translation_obj = Translation(text=text,
-                              from_language=from_language,
-                              to_language=to_language,
-                              translation=', '.join(translations['text']))
-            # create object
-            translation_obj.save()
+    def _api_translate(self, text, from_language, to_language):
+        """Get translation from external API."""
+        translation = self.translation_api.translate(text, from_language, to_language)
 
-        return translation_obj
+        # TODO: errors
+        if not translation:
+            return None
+
+        # Create and save object to db
+        obj = Translation(text=text,
+                          from_language=from_language,
+                          to_language=to_language,
+                          # comma separated if more than one translation is returned
+                          translation=', '.join(translation['text']))
+        # create object
+        obj.save()
+        return obj
+
+    def get_translation(self, text, from_language, to_language):
+        """Get translation from database, or fallback to external API."""
+        translation = self._database_translate(text, from_language, to_language)
+        if not translation:
+            print('api call made')
+            translation = self._api_translate(text, from_language, to_language)
+
+        return translation
 
 
 ### IN API FILE
