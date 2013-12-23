314bd9a6513f10aeff7e02f90a309f2593a07c6e
alcesleo <lagginglion@gmail.com>
2013-12-23 17:42:55 +0100

    PythonAnywhere server configuration

--- vocabool/wsgi.py
+++ vocabool/wsgi.py
@@ -1,14 +1,20 @@
 """
-WSGI config for vocabool project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+WSGI config for PythonAnywhere using Python3.3 in a virtualenv.
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vocabool.settings")
+# activate virtualenv
+activate_this = '/home/alcesleo/.virtualenvs/vocabool/bin/activate_this.py'
+with open(activate_this) as f:
+    code = compile(f.read(), activate_this, 'exec')
+    exec(code, dict(__file__=activate_this))
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+import os
+import sys
+
+path = '/home/alcesleo/vocabool'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vocabool.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
