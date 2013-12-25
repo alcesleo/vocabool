2fb5a81cae80a2c47dca99f57279fec047ee6f0c
alcesleo <lagginglion@gmail.com>
2013-12-25 14:16:07 +0100

    Dev and production WSGI

---
+++ vocabool/wsgi-pythonanywhere.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for PythonAnywhere using Python3.3 in a virtualenv.
+"""
+
+# activate virtualenv
+activate_this = '/home/alcesleo/.virtualenvs/vocabool/bin/activate_this.py'
+with open(activate_this) as f:
+    code = compile(f.read(), activate_this, 'exec')
+    exec(code, dict(__file__=activate_this))
+
+import os
+import sys
+
+path = '/home/alcesleo/vocabool'
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vocabool.settings'
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
--- vocabool/wsgi.py
+++ vocabool/wsgi.py
@@ -1,20 +1,14 @@
 """
-WSGI config for PythonAnywhere using Python3.3 in a virtualenv.
+WSGI config for vocabool project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-# activate virtualenv
-activate_this = '/home/alcesleo/.virtualenvs/vocabool/bin/activate_this.py'
-with open(activate_this) as f:
-    code = compile(f.read(), activate_this, 'exec')
-    exec(code, dict(__file__=activate_this))
-
 import os
-import sys
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vocabool.settings")
 
-path = '/home/alcesleo/vocabool'
-if path not in sys.path:
-    sys.path.append(path)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'vocabool.settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
