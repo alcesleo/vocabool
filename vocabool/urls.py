96dd81a389cdfd611d1a989700e1dd018615cd49
alcesleo <lagginglion@gmail.com>
2013-12-26 16:55:04 +0100

    Nicer urls to the client app

--- vocabool/urls.py
+++ vocabool/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 from django.contrib import admin
@@ -7,6 +8,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include('vocabool.api.urls')),
-    # all other matches gets the index
-    url(r'', TemplateView.as_view(template_name='client/index.html')),
+
+    # route to the backbone app
+    url(r'^$', lambda r: HttpResponseRedirect('app/')),
+    url(r'^app/', TemplateView.as_view(template_name='client/index.html')),
 )
