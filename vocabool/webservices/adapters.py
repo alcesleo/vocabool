10fc5b3fd1c8b8ed2b8a8f71d03759a6cc67346b
alcesleo <lagginglion@gmail.com>
2013-12-27 18:06:59 +0100

    Ditched Google translate for Wiktionary

--- vocabool/webservices/adapters.py
+++ vocabool/webservices/adapters.py
@@ -72,30 +72,6 @@ class WiktionaryAPIAdapter():
         return Definition(text=text, language=language, definition=definition_text)
 
 
-class GoogleDictionaryAPIAdapter():
-
-    def __init__(self):
-        self.definitions_api = GoogleDictionaryAPI()
-
-    def define(self, text, source_language, target_language):
-        data = self.definitions_api.define(
-            text, source_language, target_language)
-
-        if not data:
-            return None
-
-        definition_text = self._parse_data(data)
-        obj = Definition(text=text, language=target_language,
-                         definition=definition_text)
-        return obj
-
-    def _parse_data(self, data):
-        # FIXME: better parsing
-        # TODO: exceptions
-        print(data)
-        return data['primaries'][0]['entries'][0]['terms'][0]['text']
-
-
 class YandexTranslateAPIAdapter():
 
     def __init__(self):
--- vocabool/webservices/apis.py
+++ vocabool/webservices/apis.py
@@ -26,6 +26,7 @@ class YandexTranslateAPI():
         data = response.content.decode('utf-8')
         return json.loads(data)
 
+
 class WiktionaryAPI():
     """
     One of the absolutely best sources of data, has almost everything
@@ -43,35 +44,3 @@ class WiktionaryAPI():
         response = requests.get(url, headers={'User-Agent': 'Vocabool (https://github.com/alcesleo/vocabool; lagginglion@gmail.com)'})
         data = response.content.decode('utf-8')
         return json.loads(data)
-
-
-class GoogleDictionaryAPI():
-    """
-    WARNING: This API is deprecated and has no documentation.
-    However it is still one of the most simple dictionary API:s I
-    could find, and I'm very happy as long as it works.
-    """
-
-    def _jsonp_to_json(self, data):
-        """
-        Convert jsonp to json,
-        removing everything before first {,
-        and everything after last }.
-        """
-        left = data.find('{')
-        right = data.rfind('}') + 1
-        return data[left:right]
-
-    def define(self, text, source_language, target_language):
-
-        base_url = 'http://www.google.com/dictionary/json'
-
-        # only supports jsonp
-        data = '?callback=c&sl={0}&tl={1}&q={2}'.format(source_language, target_language, text)
-        url = base_url + data
-
-        response = requests.get(url)
-        data = response.content.decode('unicode-escape')
-        data = self._jsonp_to_json(data)
-        return json.loads(data)
-
