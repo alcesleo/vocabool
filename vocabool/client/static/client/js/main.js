95e3dbcd52a5cc49bf0c667be8cfd67a7a670448
alcesleo <lagginglion@gmail.com>
2014-01-15 16:07:54 +0100

    Cleaned up main

--- vocabool/client/static/client/js/main.js
+++ vocabool/client/static/client/js/main.js
@@ -1,31 +1,9 @@
-
 $(function () {
     'use strict';
-    // Initialize namespace
     window.VB = window.VB || {};
     VB.app = VB.app || {}; // holds global intances
-    // window.vocabularies = new VB.Collections.VocabularyCollection();
-    // window.listView = new VB.Views.VocabularyList({ collection: vocabularies });
-    // window.appView = new VB.Views.App({ el: '#view' });
-
-    // function initSpinner(options) {
-    //     options = options || {};
-    //     $("#loading").spin(options).hide();
-    //     $('#loading').ajaxStart(function(){ $(this).fadeIn(); });
-    //     $('#loading').ajaxComplete(function(){ $(this).fadeOut(); });
-    // }
-
-    // TODO: elsewhere?
-    // initSpinner();
-
-    // Router
 
     VB.helpers.enableCsrf();
     VB.app.router = new VB.Router();
     Backbone.history.start({pushState: false, root: '/app/'})
-
 });
-
-
-
-
